9271478b279bebf9ae05a05bdbd80477acec5d44
Aditya <adityagupta765469@gmail.com>
2023-06-30 14:49:35 +0530

    first commit

---
+++ pages/api/generate.js
@@ -0,0 +1,64 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+   const text = req.body.text || "";
+   if (text.trim().length === 0) {
+     res.status(400).json({
+       error: {
+         message: "Please enter a valid text",
+       },
+     });
+     return;
+   }
+  if (text.trim().length <= 60) {
+     res.status(400).json({
+       error: {
+         message: "Please enter more text",
+       },
+     });
+     return;
+  }
+  
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generateResponse(text),
+      temperature: 0.7,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+    res.status(200).json({ result: response.data.choices[0].text });
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+const generateResponse = (text) => {
+  return `Summarize this in 40 words:\n\n${text}`;
+}
---
+++ pages/index.js
@@ -0,0 +1,57 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: text }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setText("");
+    } catch(error) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>OpenAI Quickstart</title>
+        <link rel="icon" href="/dog.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <h3>Summarise this text</h3>
+        <form onSubmit={onSubmit}>
+          <textarea
+            type="text"
+            name="text"
+            rows={10}
+            placeholder="Enter a text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input type="submit" value="Generate summarised text" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
